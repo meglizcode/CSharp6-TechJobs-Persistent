82fffe4fce0ea6c1434802abd6cdbede2b5da52d
meglizcode <meglizjudd@gmail.com>
2024-06-26 17:24:56 -0500

    Task 2: Complete

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -3,8 +3,12 @@
 
 
 --Part 1: List the columns and their data types in the Jobs table.
+-- Id int
+-- Name longtext
+-- EmployerID int
 
 --Part 2: Write a query to list the names of the employers in St. Louis City.
+-- SELECT * FROM employers WHERE location = "St. Louis City";
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
